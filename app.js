276c217a6e3901c6b801337f5b5749daf7f5a14b
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-14 22:08:28 +0530

    new server given

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 
+const http= require('http');
+
+const createApplication = require('express');
+const app = createApplication();
+
+const server = http.createServer(app);
+const PORT= 3002;
+server.listen(PORT , ()=>{
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
